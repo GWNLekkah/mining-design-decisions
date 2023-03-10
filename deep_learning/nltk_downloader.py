baafc60460b9065981dc2b2846acb792d8de050c
Jesse Maarleveld <jessemaarleveld@gmail.com>
2023-03-10 18:40:17 +0100

    Add Dockerfile

--- deep_learning/nltk_downloader.py
+++
@@ -1,8 +0,0 @@
-# Helper file for installing NLTK stuff
-
-import nltk
-nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('wordnet')
-nltk.download('omw-1.4')
-nltk.download('averaged_perceptron_tagger')
