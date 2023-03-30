00f335ea877befbff6155047c2129212e5008d81
Arjan <a.j.dekker.5@student.rug.nl>
2023-03-30 09:42:51 +0200

    Fixed minor bugs

--- deep_learning/dl_manager/data_manager_bootstrap.py
+++ deep_learning/dl_manager/data_manager_bootstrap.py
@@ -16,19 +16,5 @@ def get_raw_text_file_name() -> str:
     else:
         path = f'{conf.get("system.storage.file_prefix")}_raw_words'
     os.makedirs(path, exist_ok=True)
-    options = []
-    params = conf.get('make-features.params')
-    keys = ('max-len',
-            'disable-lowercase',
-            'disable-stopwords',
-            'disable-stemmnig',
-            'use-lemmatization',
-            'use-pos', )
-    for key in keys:
-        if key in params['default']:
-            options.append(f'{key}_{params["default"][key]}')
-    if conf.get('make-features.ontology-classes'):
-        options.append('with_ontologies')
-    options.sort()
-    filename = os.path.join(path, f'{conf.get("system.storage.file_prefix")}_raw_words_{"__".join(options)}.json')
+    filename = os.path.join(path, f'{conf.get("system.storage.file_prefix")}_raw_words.json')
     return filename
