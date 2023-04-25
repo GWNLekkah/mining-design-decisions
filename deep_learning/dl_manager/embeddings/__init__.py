0ba3bb5b3d870a853ebcb3fdabc329b1df18c775
Jesse Maarleveld <jessemaarleveld@gmail.com>
2023-04-25 11:38:46 +0200

    Create BOW dictionary generator

--- deep_learning/dl_manager/embeddings/__init__.py
+++ deep_learning/dl_manager/embeddings/__init__.py
@@ -1,10 +1,12 @@
 from .embedding_generator import AbstractEmbeddingGenerator
 from .word2vec import Word2VecGenerator
 from .doc2vec import Doc2VecGenerator
+from .dictionary import DictionaryGenerator
 
 _gens = [
     Word2VecGenerator,
-    Doc2VecGenerator
+    Doc2VecGenerator,
+    DictionaryGenerator
 ]
 
 generators = {g.__name__: g for g in _gens}
---
+++ deep_learning/dl_manager/embeddings/dictionary.py
@@ -0,0 +1,37 @@
+import collections
+import json
+import pathlib
+
+from .embedding_generator import AbstractEmbeddingGenerator
+from ..config import Argument, IntArgument
+
+
+class DictionaryGenerator(AbstractEmbeddingGenerator):
+
+    def generate_embedding(self, issues: list[list[str]], path: pathlib.Path):
+        # Generate a frequency table
+        frequencies = collections.defaultdict(int)
+        for issue in issues:
+            for word in set(issue):
+                frequencies[word] += 1
+        # Select words to be contained in dictionary
+        threshold = self.params['min-doc-count']
+        dictionary = [
+            word for word, frequency in frequencies.items()
+            if frequency >= threshold
+        ]
+        # Let's make the dictionary sorted
+        dictionary.sort()
+        # Save dictionary
+        with open(path, 'w') as file:
+            json.dump(dictionary, file)
+
+    @staticmethod
+    def get_arguments() -> dict[str, Argument]:
+        return {
+            'min-doc-count': IntArgument(
+                name='min-doc-count',
+                description='Minimum document count for a word to be included in the dictionary',
+                minimum=0
+            )
+        }
