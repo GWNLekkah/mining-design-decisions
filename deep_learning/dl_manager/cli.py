e73a3e9239105c6c94b3a647da66ba3130580443
Jesse Maarleveld <jessemaarleveld@gmail.com>
2023-05-18 15:41:14 +0200

    Fix

--- deep_learning/dl_manager/cli.py
+++ deep_learning/dl_manager/cli.py
@@ -145,7 +145,7 @@ def setup_app_constraints(app):
         'run.k-cross', 'run.test-with-training-data'
     )
     app.add_constraint(
-        lambda ontology_id, apply_ontology, models: ('OntologyFeatures' not in models and not apply_ontology) or ontology_id != '',
+        lambda ontology_id, apply_ontology, models: True if 'OntologyFeatures' not in models and not apply_ontology else ontology_id != '',
         'Ontology class file must be given when applying ontology classes or using ontology features',
         'run.ontology-classes', 'run.apply-ontology-classes', 'run.classifier'
     )
